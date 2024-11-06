25d74cd0202fb4ace1c3ca1e17cfffcfddb88a90
danivaz311 <danivaz311@gmail.com>
2024-11-06 13:55:18 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function loguear() {
     var user = document.getElementById("Usuario").value;
     var password = document.getElementById("contrasena").value;
 
-    // Cambia estas credenciales por un backend real
+    // Cambia estas credenciales por un backend real o por un método más seguro
     const validUser = "10155002-S";
     const validPassword = "93699";
 
@@ -20,29 +20,5 @@ function loguear() {
         alert("Datos incorrectos");
     }
 
-    // Prevenir el envío del formulario
-    return false;
-}
+    // Prev
 
-// Verifica si el usuario está logueado al cargar la página
-window.onload = function() {
-    // Si no está logueado, redirige a la página de inicio de sesión
-    if (!sessionStorage.getItem("loggedIn")) {
-        window.location.href = "index.html"; // Redirige a la página de login si no está logueado
-    } else {
-        // Muestra la información del usuario en la página protegida
-        const usuario = sessionStorage.getItem("usuario");
-        const contrasena = sessionStorage.getItem("contrasena");
-        document.getElementById("user-info").innerHTML = `<h3>Usuario: ${usuario}</h3><h3>Contraseña: ${contrasena}</h3>`;
-    }
-};
-
-// Función para cerrar sesión
-function cerrarSesion() {
-    sessionStorage.removeItem("loggedIn"); // Elimina el estado de sesión
-    sessionStorage.removeItem("usuario"); // Elimina el usuario
-    sessionStorage.removeItem("contrasena"); // Elimina la contraseña
-
-    // Redirige a la página de inicio de sesión después de cerrar sesión
-    window.location.href = "index.html"; 
-}

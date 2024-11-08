c97c375392897753533d9659072a85c5a9a04ecf
danivaz311 <danivaz311@gmail.com>
2024-11-08 12:10:55 -0600

    Update main.js

--- main.js
+++ main.js
@@ -29,9 +29,6 @@ window.onload = function() {
     } else {
         // Evita el almacenamiento en caché de esta página
         history.pushState(null, null, location.href);
-        window.onpopstate = function() {
-            history.pushState(null, null, location.href);
-        };
     }
 };
 
